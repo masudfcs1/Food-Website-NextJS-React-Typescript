a0bd94bba4180e3907db07b50c87d5a60cd035d9
masud rana <masudfcs1@gmail.com>
2023-08-26 02:53:21 +0600

    Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,6 +13,25 @@ const Hero = () => {
         alt="hero bg"
       />
       <Navbar />
+
+      <div className=" container h-[calc(100vh-120vh)] grid items-center ">
+        <div className=" space-y-4 bg-[#ffffff98] w-fit p-4 ">
+          <p className=" uppercase font-medium">Wide options of choice</p>
+          <h2 className=" text-4xl sm:text-6xl font-bold">
+            Delicious <span className=" text-green-500">Food</span>
+          </h2>
+          <p className=" text-gray-700 text-[14px] sm:text-[16px] ">
+            {" "}
+            Delicious food color , aroma and taste. <br /> What are you waiting
+            for?
+          </p>
+
+          <button className=" bg-green-500 px-6 hover:bg-green-600 py-2 rounded-3xl justify-center items-center text-[18px] sm:text-[17px] ">
+            {" "}
+            View more
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "../../components/Hero";
+import Feature from "../../components/Feature";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Feature />
     </main>
   );
 }
