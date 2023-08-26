9bf01b1db3b089ec7e005bbad4fb90d79d2a790f
masud rana <masudfcs1@gmail.com>
2023-08-26 18:51:43 +0600

    Features

---
+++ components/Dash.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Dash() {
+  return <div className=" bg-green-500 w-[70px] h-[6px] mt-10 " />;
+}
--- components/Feature.tsx
+++ components/Feature.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import Dash from "./Dash";
+import Image from "next/image";
+import imgg1 from "../public/grid__1.png";
 export default function Feature() {
   return (
     <div className=" container pt-40">
@@ -13,6 +15,73 @@ export default function Feature() {
         deserunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
         a!
       </p>
+
+      <Dash />
+
+      <div className=" grid md:grid-cols-[1fr,37%,1fr] gap-16 mt-10">
+        {/* 1st image */}
+        <div className=" w-fit mx-auto self-end">
+          <Image
+            alt="img"
+            src="/grid__1.png"
+            className=" w-[100%] rounded-xl max-w-[400px] sm:max-w-full h-auto shadow-2xl "
+            width={300}
+            height={600}
+          />
+          {/* <Image src={imgg1} alt="grid image" width={300} height={600} /> */}
+
+          <div className=" space-y-4">
+            <Dash />
+            <h2 className=" font-medium text-xl">Vegetable Salad</h2>
+            <p className=" text-gray-700 text-[14px] xl:text-[16px]">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Repudiandae, illo!
+            </p>
+          </div>
+        </div>
+
+        {/* 2nd image */}
+        <div className=" w-fit mx-auto ">
+          <Image
+            alt="img"
+            src="/grid__2.png"
+            className=" w-[100%] rounded-xl max-w-[400px] sm:max-w-full h-auto shadow-2xl "
+            width={500}
+            height={900}
+          />
+          {/* <Image src={imgg1} alt="grid image" width={300} height={600} /> */}
+
+          <div className=" space-y-4">
+            <Dash />
+            <h2 className=" font-medium text-xl">Baked apples</h2>
+            <p className=" text-gray-700 text-[14px] xl:text-[16px]">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Repudiandae, illo!
+            </p>
+          </div>
+        </div>
+
+        {/* 3rd image */}
+        <div className=" w-fit mx-auto self-end">
+          <Image
+            alt="img"
+            src="/grid__1.png"
+            className=" w-[100%] rounded-xl max-w-[400px] sm:max-w-full h-auto shadow-2xl "
+            width={300}
+            height={600}
+          />
+          {/* <Image src={imgg1} alt="grid image" width={300} height={600} /> */}
+
+          <div className=" space-y-4">
+            <Dash />
+            <h2 className=" font-medium text-xl">Cherries chicken</h2>
+            <p className=" text-gray-700 text-[14px] xl:text-[16px]">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Repudiandae, illo!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
